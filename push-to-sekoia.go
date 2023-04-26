7539d7d0525cc29962dd1c0f7939dbab9e58a85d
em3 <121884884+emgrandb@users.noreply.github.com>
2023-04-26 11:19:37 +0200

    Create push-to-sekoia.go

---
+++ push-to-sekoia.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+        "bytes"
+        "fmt"
+        "io"
+        "net/http"
+        "os"
+        "strings"
+)
+
+func main() {
+        stdin, err := io.ReadAll(os.Stdin)
+
+        if err != nil {
+                panic(err) 
+        }
+        str := string(stdin) 
+        fmt.Println(strings.TrimSuffix(str, "\n")) 
+
+        url := "https://intake.sekoia.io/plain" 
+
+        var data = []byte(str) 
+        req, err := http.NewRequest("POST", url, bytes.NewBuffer(data)) 
+        req.Header.Set("X-SEKOIAIO-INTAKE-KEY", os.Args[1]) 
+        req.Header.Set("Content-Type", "application/json") 
+
+        client := &http.Client{} 
+        resp, err := client.Do(req)
+        if err != nil {
+                panic(err) 
+        }
+        defer resp.Body.Close()
+
+        fmt.Println("response Status:", resp.Status) 
+        fmt.Println("response Headers:", resp.Header)
+        body, _ := io.ReadAll(resp.Body)
+        fmt.Println("response Body:", string(body))
+}
