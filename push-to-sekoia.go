d04a94a7a7b45796be7953d028b38586775f7142
Olivier Moreau <olivier.moreau@aktea.fr>
2023-05-05 16:26:13 +0200

    cleanup pre fuix

--- push-to-sekoia.go
+++ push-to-sekoia.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-        "bytes"
-        "fmt"
-        "io"
-        "net/http"
-        "os"
-        "strings"
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-        // Function to put the STDIN in a var called str
-        stdin, err := io.ReadAll(os.Stdin)
+	// Function to put the STDIN in a var called str
+	stdin, err := io.ReadAll(os.Stdin)
 
-        if err != nil {
-                panic(err) 
-        }
-        str := string(stdin) 
-        fmt.Println(strings.TrimSuffix(str, "\n")) 
-        
-        // Fuction to make a POST request 
-        url := "https://intake.sekoia.io/plain" 
+	if err != nil {
+		panic(err)
+	}
+	str := string(stdin)
+	fmt.Println(strings.TrimSuffix(str, "\n"))
 
-        var data = []byte(str) 
-        req, err := http.NewRequest("POST", url, bytes.NewBuffer(data)) 
-        req.Header.Set("X-SEKOIAIO-INTAKE-KEY", os.Args[1]) 
-        req.Header.Set("Content-Type", "application/json") 
+	// Fuction to make a POST request
+	url := "https://intake.sekoia.io/plain"
 
-        client := &http.Client{} 
-        resp, err := client.Do(req)
-        if err != nil {
-                panic(err) 
-        }
-        defer resp.Body.Close()
+	var data = []byte(str)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	req.Header.Set("X-SEKOIAIO-INTAKE-KEY", os.Args[1])
+	req.Header.Set("Content-Type", "application/json")
 
-        fmt.Println("response Status:", resp.Status) 
-        fmt.Println("response Headers:", resp.Header)
-        body, _ := io.ReadAll(resp.Body)
-        fmt.Println("response Body:", string(body))
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
 }
