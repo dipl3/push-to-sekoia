7fe5eee599480902c13ca26fccb3c33d46709f6a
Olivier Moreau <olivier.moreau@aktea.fr>
2023-05-09 11:16:13 +0200

    works

--- push-to-sekoia.go
+++ push-to-sekoia.go
@@ -1,41 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"strings"
+	"time"
 )
 
+var in *bufio.Reader
+
+func receivedLog(stdin io.Reader) (string, error) {
+	if in == nil {
+		in = bufio.NewReader(stdin)
+	}
+	logLine, err := in.ReadString('\n')
+	return logLine, err
+}
+
 func main() {
+
 	// Function to put the STDIN in a var called str
-	stdin, err := io.ReadAll(os.Stdin)
+	loglign, err := receivedLog(os.Stdin)
 
 	if err != nil {
 		panic(err)
 	}
-	str := string(stdin)
-	fmt.Println(strings.TrimSuffix(str, "\n"))
+	//fmt.Println(strings.TrimSuffix(str, "\n"))
 
 	// Fuction to make a POST request
 	url := "https://intake.sekoia.io/plain"
 
-	var data = []byte(str)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(loglign)))
 	req.Header.Set("X-SEKOIAIO-INTAKE-KEY", os.Args[1])
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept-Encoding", "*")
 
-	client := &http.Client{}
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
 	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Println("panic")
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		fmt.Println(resp.StatusCode)
+		os.Exit(resp.StatusCode)
+	}
 	body, _ := io.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
+
+	fmt.Printf("%s: %s\n", time.Now(), string(body))
+
+	// TODO proper logging
+	os.Exit(0)
 }
